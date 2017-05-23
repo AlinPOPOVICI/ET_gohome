c8091473e3ab966f2815f245a35010bb381d28cd
AlinPOPOVICI <popovici.alin@gmail.com>
2017-05-23 23:26:19 +0300

    Update ceas_v1.v

--- ceas_v1.v
+++ ceas_v1.v
@@ -21,6 +21,7 @@
 module ceas_top(
 		clock,
 		reset,
+		i_rx_serial,
 		semnal_setare,
 		semnal_setare_a,
 		semnal_stop,
@@ -32,6 +33,7 @@ module ceas_top(
     );
 		input clock;
 		input reset;
+		input i_rx_serial;
 		input semnal_setare;
 		input semnal_setare_a;
 		input semnal_stop;
@@ -49,16 +51,18 @@ wire [4:0] ore_setare_out;
 wire [5:0] minute_setare_out;
 wire [4:0] ore_counter_out;
 wire [5:0] minute_counter_out;
+wire [4:0] ore_uart_out;
+wire [5:0] minute_uart_out;
 
 counter_timp CT(
      .timp_ore1(ore_setare_out),
 	  .timp_minute1(minute_setare_out), 
-	  .timp_ore2(),
-	  .timp_minute2(),
+	  .timp_ore2(ore_uart_out),
+	  .timp_minute2(minute_uart_out),
 	  .out_ore(ore_counter_out),
 	  .out_minute(minute_counter_out),
      .load_1(load_timp_manual),
-     .load_2(),
+     .load_2(load_timp_uart),
 	  .clock(clock),
 	  .reset(reset)
     );
@@ -88,6 +92,15 @@ setare S(
 	 .load_alarma(load_alarma),
 	 .load_timp(load_timp_manual)
     );
+	 
+uart_top U( // nush daca merge 
+	.reset(reset),
+	.clock(clock),
+	.i_rx_serial(i_rx_serial),
+	.minute(minute_uart_out),
+	.ore(ore_uart_out),
+	.load(load_timp_uart)	
+	);	 
 	 
 alarma_top A( // nu merge complet cum trebuie 
 	 .clock(clock),
