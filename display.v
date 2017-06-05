5f53b034563dface96a6f077d886ac2295c3ca2a
AlinPOPOVICI <popovici.alin@gmail.com>
2017-06-05 22:33:23 +0300

    Update display.v

--- display.v
+++ display.v
@@ -24,25 +24,27 @@ module display(
 	 minute_setare,
 	 ore_setare,
 	 semnal_setare,
+	 semnal_setare_a,
 	 clock,
     digit_select,
     digit_display
     );
 	 
-	input [5:0] minute;
-    	input [4:0] ore;
-	input [5:0] minute_setare;
-   	input [4:0] ore_setare;
-	input semnal_setare;
-	input clock;
-    	output [3:0] digit_select;
-    	output [6:0] digit_display;
+	 input [5:0] minute;
+    input [4:0] ore;
+	 input [5:0] minute_setare;
+    input [4:0] ore_setare;
+	 input semnal_setare;
+	 input semnal_setare_a;
+	 input clock;
+    output [3:0] digit_select;
+    output [6:0] digit_display;
 	 
 	 reg [4:0] ore_i;
 	 reg [5:0] minute_i;
 	 
 	always@(posedge clock) begin
-		if(semnal_setare) begin  // mai bine cu assign 
+	 if(semnal_setare == 'd1 || semnal_setare_a == 'd1) begin
 		ore_i <= ore_setare;
 		minute_i <= minute_setare;
 	 end else begin
@@ -54,10 +56,10 @@ module display(
 	 
 	 display_fourdigit f2(
 	 .minute(minute_i),
-    	 .ore(ore_i),
+    .ore(ore_i),
 	 .clock(clock),
-         .digit_select(digit_select),
-         .digit_display(digit_display)
+    .digit_select(digit_select),
+    .digit_display(digit_display)
 	 );
 
 
