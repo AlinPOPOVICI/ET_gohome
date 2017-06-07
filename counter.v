e3ddb1db0870573ab2bd3a9837ad937a49e73a66
AlinPOPOVICI <popovici.alin@gmail.com>
2017-06-07 18:11:14 +0300

    Update counter.v

--- counter.v
+++ counter.v
@@ -48,29 +48,30 @@ module counter_timp(
 	 
 	 reg [5:0] minute;
 	 reg [4:0] ore;
-	 reg [5:0] durata_m;
-	 reg [5:0] out_durata_m;	 // cati cloci pt un minut 
-
+	 
+	 // cati cloci pt un minut  
+	 reg [26:0] counter;
+	 reg [26:0] counter_out;
+	 
 always @ (*) begin
 	out_ore = ore;
 	out_minute = minute;
-	out_durata_m = durata_m;
-	if(durata_m == 6'b111111) begin
-		out_durata_m = 'd0;
-		if(ore == 'd23 && minute == 'd59)  begin //la ora 23:59 se va trece la 00:00
-			out_ore = 'd0;
-			out_minute = 'd0;
-			
-		end else
-			if(minute == 'd59)  begin // la minutul 59 se va trece la 00
-				out_ore = out_ore + 'd1;
+	counter_out = counter;
+	if( counter == 'd50) begin // 50000000 pt 1 secunda timp real 
+		counter_out = 'd0;
+			if(ore == 'd23 && minute == 'd59)  begin //la ora 23:59 se va trece la 00:00
+				out_ore = 'd0;
 				out_minute = 'd0;
-			end else begin
-				out_minute = out_minute + 'd1;
+			
+			end else
+				if(minute == 'd59)  begin // la minutul 59 se va trece la 00
+					out_ore = out_ore + 'd1;
+					out_minute = 'd0;
+				end else begin
+					out_minute = out_minute + 'd1;
 		
-			end
-	 end else begin
-		out_durata_m = out_durata_m + 'd1; 
+				end
+		 
 	 end
 end
 	 
@@ -79,23 +80,22 @@ always @ (posedge clock) begin
  if (reset == 1'b1) begin
 		  minute <= 'd0;
 		  ore <= 'd0;
-		  durata_m <= 'd0;
-    end
-	 
-    else if (load_1 == 1'b1) begin
-        ore <=    timp_ore1;
+		  counter <= 'd0;
+    end else begin 
+		if (load_1 == 1'b1) begin
+		  ore <=    timp_ore1;
 		  minute <=  timp_minute1;
-		  durata_m <=  'd0;
-    end
-	 
-	 else if (load_2 == 1'b1) begin
-        ore <=    timp_ore2;
-		  minute <=  timp_minute2;
-		  durata_m <= 'd0;
-    end else begin
-			ore <= out_ore;
-			minute <= out_minute;
-			durata_m <= out_durata_m;
+		end else begin
+			if (load_2 == 1'b1) begin
+				ore <=    timp_ore2;
+				minute <=  timp_minute2;
+
+			end else begin
+				ore <= out_ore;
+				minute <= out_minute;
+				counter <= counter_out + 'd1;
+			end
+		end
 	 end 
 end	 
 
